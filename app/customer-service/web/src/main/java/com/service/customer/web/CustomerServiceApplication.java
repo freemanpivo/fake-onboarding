ce0ec26e213893a9d0d0bcb210946ed803258e09
Pedro Ivo Andrade <andradepedroivo@gmail.com>
2022-02-20 19:47:10 -0300

    :tada: add basic app structure

---
+++ app/customer-service/web/src/main/java/com/service/customer/web/CustomerServiceApplication.java
@@ -0,0 +1,16 @@
+package com.service.customer.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.service.customer"})
+public class CustomerServiceApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(CustomerServiceApplication.class);
+    }
+}
---
+++ app/customer-service/web/src/main/java/com/service/customer/web/CustomerServiceConfiguration.java
@@ -0,0 +1,8 @@
+package com.service.customer.web;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CustomerServiceConfiguration {
+
+}
